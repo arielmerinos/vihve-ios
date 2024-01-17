f07cf2fb968084caa2a419a5ad98b3d59effdf68
arielmerinos <arielmerino@ciencias.unam.mx>
2024-01-17 14:18:21 -0600

    Initial Commit

---
+++ VIHve Libre/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  VIHve Libre
+//
+//  Created by Ariel Merino on 17/01/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
